7076c024cc318c0c4638bd864278e7a168fa3c1a
“Arianna-Salvini” <arianna.salvini@gmail.com>
2024-04-03 18:23:17 +0200

    Display API response for movies' title, original title, language and everage rating

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,20 +1,29 @@
 <script>
 
-// import { state } from '../state.js'
+import { state } from '../state.js'
 
 export default {
     name: 'AppMain',
 
-    // data() {
-    //     return {
-    //         state,
-    //     }
-    // },
+    data() {
+        return {
+            state,
+        }
+    },
 }
 </script>
 
 <template>
-
+    <div class="container">
+        <ul>
+            <li v-for="(movie, index) in state.movies" :key="index">
+                <h2>{{ movie.title }}</h2>
+                <h4>{{ movie.original_title }}</h4>
+                <div class="language">{{ movie.original_language }}</div>
+                <div class="rating"> {{ movie.vote_average }}</div>
+            </li>
+        </ul>
+    </div>
 </template>
 
 <style></style>
