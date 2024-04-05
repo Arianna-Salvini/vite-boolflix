20b64e6ab7bb5f6af96aa1514afd739c5951453f
“Arianna-Salvini” <arianna.salvini@gmail.com>
2024-04-05 18:45:30 +0200

    Add footer

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -6,6 +6,24 @@ export default {
 </script>
 
 <template>
+    <div class="footer">
+        <h5>© Made by Arianna Salvini - Boolean Class119</h5>
+    </div>
 </template>
 
-<style></style>
+<style>
+.footer {
+    width: 100%;
+    height: 2.5rem;
+    padding: 0.5rem 2rem;
+    margin-top: 1rem;
+    background-image: radial-gradient(circle, #22222276, #44444476);
+    box-shadow: inset 0 0 5rem #000000, 0 0 1.5rem #000000;
+    text-align: center;
+    line-height: 1.8rem;
+    position: fixed;
+    bottom: 0;
+    left: 0;
+
+}
+</style>
